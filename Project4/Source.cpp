cf0a04b2bd8c6c2d374381850da682809cf2fb2d
Roneri8iny <98766927+Roneri8iny@users.noreply.github.com>
2022-04-23 23:14:24 +0200

    Added other motors This is according to my pins

--- Project4/Source.cpp
+++ Project4/Source.cpp
@@ -1,99 +1,130 @@
 #include<Servo.h>
-#define rightIr 6
-#define leftIr 8
-#define frontIr 7
-#define buzzer 13
-#define motor1 10
-#define motorr1 11
+#define motor1 2
+#define motorr1 3
 #define motor2  4
-#define motorr2 3
+#define motorr2 5
+#define motor3 6
+#define motorr3 7
+#define motor4 8
+#define motorr4 9
+#define rightIr 10
+#define leftIr 12
+#define frontIr 11
+#define buzzer 13
+
 Servo servo;
 
-
 void setup() {
-    // put your setup code here, to run once:
+	// put your setup code here, to run once:
 
-    pinMode(6, INPUT);
-    pinMode(8, INPUT);
-    pinMode(7, INPUT);
-    pinMode(13, OUTPUT);
-    pinMode(10, OUTPUT);
-    pinMode(11, OUTPUT);
-    pinMode(3, OUTPUT);
-    pinMode(4, OUTPUT);
-    servo.attached(A5);
-    servo.write(0);
-    int pos = 0;
+	pinMode(6, INPUT);
+	pinMode(8, INPUT);
+	pinMode(7, INPUT);
+	pinMode(13, OUTPUT);
+	pinMode(10, OUTPUT);
+	pinMode(11, OUTPUT);
+	pinMode(3, OUTPUT);
+	pinMode(4, OUTPUT);
+	servo.attach(A0);
+	servo.write(0);
+
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+	// put your main code here, to run repeatedly:
 
+	if (digitalRead(rightIr) && !(digitalRead(leftIr)) && !(digitalRead(frontIr)))
+	{
+		digitalWrite(buzzer, HIGH);
+		servo.write(60);
+		digitalWrite(motor1, HIGH);
+		digitalWrite(motorr1, LOW);
+		digitalWrite(motor2, HIGH);
+		digitalWrite(motorr2, LOW);
+		digitalWrite(motor3, HIGH);
+		digitalWrite(motorr3, LOW);
+		digitalWrite(motor4, HIGH);
+		digitalWrite(motorr4, LOW);
+	}
+	else if (!(digitalRead(rightIr)) && digitalRead(leftIr) && !(digitalRead(frontIr)))
+	{
+		digitalWrite(buzzer, HIGH);
+		servo.write(150);
+		digitalWrite(motor1, HIGH);
+		digitalWrite(motorr1, LOW);
+		digitalWrite(motor2, HIGH);
+		digitalWrite(motorr2, LOW);
+		digitalWrite(motor3, HIGH);
+		digitalWrite(motorr3, LOW);
+		digitalWrite(motor4, HIGH);
+		digitalWrite(motorr4, LOW);
+	}
+	else if (!(digitalRead(rightIr)) && !(digitalRead(leftIr)) && (digitalRead(frontIr)))
+	{
+		digitalWrite(buzzer, HIGH);
+		servo.write(90);
+		digitalWrite(motor1, HIGH);
+		digitalWrite(motorr1, LOW);
+		digitalWrite(motor2, HIGH);
+		digitalWrite(motorr2, LOW);
+		digitalWrite(motor3, HIGH);
+		digitalWrite(motorr3, LOW);
+		digitalWrite(motor4, HIGH);
+		digitalWrite(motorr4, LOW);
+	}
+	else if (!(digitalRead(rightIr)) && (digitalRead(leftIr)) && (digitalRead(frontIr)))
+	{
+		digitalWrite(buzzer, HIGH);
+		servo.write(120);
+		digitalWrite(motor1, HIGH);
+		digitalWrite(motorr1, LOW);
+		digitalWrite(motor2, HIGH);
+		digitalWrite(motorr2, LOW);
+		digitalWrite(motor3, HIGH);
+		digitalWrite(motorr3, LOW);
+		digitalWrite(motor4, HIGH);
+		digitalWrite(motorr4, LOW);
+	}
 
-    if (digitalRead(rightIr) && !(digitalRead(leftIr)) && !(digitalRead(frontIr))
-    {
-        digitalWrite(buzzer, HIGH);
-            servo.write(60);
-            digitalWrite(motor1, HIGH);
-            digitalWrite(motor1, LOW);
-            digitalWrite(motor2, HIGH);
-            digitalWrite(motorR2, LOW);
-    }
-    else if (!(digitalRead(righIr)) && digitalRead(leftIr) && !(digitalRead(frontIr))
-    {
-        digitalWrite(buzzer, HIGH);
-            servo.write(150);
-            digitalWrite(motor1, HIGH);
-            digitalWrite(motor1, LOW);
-            digitalWrite(motor2, HIGH);
-            digitalWrite(motorR2, LOW);
-    }
-    else if (!(digitalRead(righIr)) && !(digitalRead(leftIr)) && (digitalRead(frontIr))
-    {
-        digitalWrite(buzzer, HIGH);
-            servo.write(90);
-            digitalWrite(motor1, HIGH);
-            digitalWrite(motor1, LOW);
-            digitalWrite(motor2, HIGH);
-            digitalWrite(motorR2, LOW);
-    }
-    else if (!(digitalRead(righIr)) && (digitalRead(leftIr)) && (digitalRead(frontIr))
-    {
-        digitalWrite(buzzer, HIGH);
-            servo.write(120);
-            digitalWrite(motor1, HIGH);
-            digitalWrite(motor1, LOW);
-            digitalWrite(motor2, HIGH);
-            digitalWrite(motorR2, LOW);
-    }
+	else if ((digitalRead(rightIr)) && !(digitalRead(leftIr)) && (digitalRead(frontIr)))
+	{
+		digitalWrite(buzzer, HIGH);
+		servo.write(75);
+		digitalWrite(motor1, HIGH);
+		digitalWrite(motorr1, LOW);
+		digitalWrite(motor2, HIGH);
+		digitalWrite(motorr2, LOW);
+		digitalWrite(motor3, HIGH);
+		digitalWrite(motorr3, LOW);
+		digitalWrite(motor4, HIGH);
+		digitalWrite(motorr4, LOW);
+	}
 
-    else if ((digitalRead(righIr)) && !(digitalRead(leftIr)) && (digitalRead(frontIr))
-    {
-        digitalWrite(buzzer, HIGH);
-            servo.write(75);
-            digitalWrite(motor1, HIGH);
-            digitalWrite(motor1, LOW);
-            digitalWrite(motor2, HIGH);
-            digitalWrite(motorR2, LOW);
-    }
+	else if ((digitalRead(rightIr)) && (digitalRead(leftIr)) && (digitalRead(frontIr)))
+	{
+		digitalWrite(buzzer, HIGH);
+		servo.write(75);
+		digitalWrite(motor1, HIGH);
+		digitalWrite(motorr1, LOW);
+		digitalWrite(motor2, HIGH);
+		digitalWrite(motorr2, LOW);
+		digitalWrite(motor3, HIGH);
+		digitalWrite(motorr3, LOW);
+		digitalWrite(motor4, HIGH);
+		digitalWrite(motorr4, LOW);
+	}
 
-    else if ((digitalRead(righIr)) && (digitalRead(leftIr)) && (digitalRead(frontIr))
-    {
-        digitalWrite(buzzer, HIGH);
-            servo.write(75);
-            digitalWrite(motor1, HIGH);
-            digitalWrite(motor1, LOW);
-            digitalWrite(motor2, HIGH);
-            digitalWrite(motorR2, LOW);
-    }
-
-    else if (!(digitalRead(righIr)) && !(digitalRead(leftIr)) && !(digitalRead(frontIr))
-    {
-        digitalWrite(buzzer, LOW);
-            servo.write(0);
-            digitalWrite(motor1, LOW);
-            digitalWrite(motor1, LOW);
-            digitalWrite(motor2, LOW);
-            digitalWrite(motorR2, LOW);
-    }
+	else
+	{
+		digitalWrite(buzzer, LOW);
+		servo.write(0);
+		digitalWrite(motor1, LOW);
+		digitalWrite(motorr1, LOW);
+		digitalWrite(motor2, LOW);
+		digitalWrite(motorr2, LOW);
+		digitalWrite(motor3, LOW);
+		digitalWrite(motorr3, LOW);
+		digitalWrite(motor4, LOW);
+		digitalWrite(motorr4, LOW);
+	}
 }
